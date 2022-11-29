93cc06d469198ebe6ec6790fa455ec8c592e4bc8
EndiSh <hoendere@gmail.com>
2022-11-29 22:25:10 +0100

    Api Version

---
+++ My-books/Controllers/v1/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_books.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("1.2")]
+    [ApiVersion("1.3")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("get-test-data"), MapToApiVersion("1.1")]
+        public IActionResult Getv1()
+        {
+            return Ok("This is version V1");
+        }
+        [HttpGet("get-test-data"), MapToApiVersion("1.2")]
+        public IActionResult Getv12()
+        {
+            return Ok("This is version V1.2");
+        }
+        [HttpGet("get-test-data"), MapToApiVersion("1.3")]
+        public IActionResult Getv13()
+        {
+            return Ok("This is version V1.3");
+        }
+
+    }
+}
---
+++ My-books/Data/Services/PublisherService.cs
@@ -0,0 +1,95 @@
+﻿using My_books.Data.Models;
+using My_books.Data.ViewModels;
+using My_books.Exceptions;
+using My_books.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace My_books.Data.Services
+{
+    public class PublisherService
+    {
+        private AppDbContext _context;
+        public PublisherService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Publisher> GetAllPublishers(string sortBy, string search, int? pageNumber) 
+        {
+            var allPublishers = _context.Publishers.OrderBy(n => n.Name).ToList();
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                switch (sortBy)
+                {
+                    case "name_desc":
+                        allPublishers = allPublishers.OrderByDescending(n => n.Name).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                allPublishers = allPublishers.Where(n => n.Name.Contains(search,StringComparison.CurrentCultureIgnoreCase)).ToList(); 
+            }
+
+            //Paging
+            int pageSize = 5;
+            allPublishers = PaginatedList<Publisher>.Create(allPublishers.AsQueryable(), pageNumber ?? 1, pageSize);
+
+            return allPublishers;
+        } 
+
+        public Publisher AddPublisher(PublisherVM publisher)
+        {
+            if (StringStartWithNumber(publisher.Name)) throw new PublisherNameException("Name starts with number", publisher.Name); //Check if start with digit throw exception
+
+            var _publisher = new Publisher()
+            {
+                Name = publisher.Name
+                
+            };
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+            return _publisher;
+        }
+
+        public Publisher GetPublisherById(int id) => _context.Publishers.FirstOrDefault(n => n.Id == id);
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisher = _context.Publishers.Where(n => n.Id == publisherId).Select(n => new PublisherWithBooksAndAuthorsVM()
+            {
+                Name = n.Name,
+                BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                {
+                    BookName = n.Title,
+                    BookAuthors = n.Book_Authors.Select(n => n.Author.FullName).ToList()
+                }).ToList()
+            }).FirstOrDefault();
+
+            return _publisher;
+        }
+
+        public void DeletePublisherById(int id)
+        {
+            var publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+            if (publisher != null)
+            {
+                _context.Publishers.Remove(publisher);
+                _context.SaveChanges();
+            }
+        }
+
+        private bool StringStartWithNumber(string name)
+        {
+            return Regex.IsMatch(name, @"^\d"); //start with digit (return true if start with digit, and false if it doesn't)
+        }
+    }
+}
